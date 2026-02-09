3316f4d80dc75e051e5b8c59fbf7353abbaf9c57
towersness <nottowers@gmail.com>
2026-02-09 17:06:54 -0600

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -1,1 +1,33 @@
-<h1>The Ranks:</h1>\n<ul>\n  <li>Best of the Best</li>\n  <li>S+</li>\n  <li>S</li>\n  <li>S-</li>\n  <li>A+</li>\n  <li>A</li>\n  <li>A-</li>\n  <li>B+</li>\n  <li>B</li>\n  <li>B-</li>\n  <li>C+</li>\n</ul>
+import Spinner from '../components/Spinner.js';
+
+export default {
+  components: {
+    Spinner,
+  },
+  data: () => ({
+    loading: false,
+  }),
+  template: `
+    <main class="page-leaderboard-container">
+      <div class="page-leaderboard">
+        <div class="ranks-container">
+          <h2>The Ranks:</h2>
+          <div class="ranks-list">
+            <div class="rank-item">Best of the Best</div>
+            <div class="rank-item">S+</div>
+            <div class="rank-item">S</div>
+            <div class="rank-item">S-</div>
+            <div class="rank-item">A+</div>
+            <div class="rank-item">A</div>
+            <div class="rank-item">A-</div>
+            <div class="rank-item">B+</div>
+            <div class="rank-item">B</div>
+            <div class="rank-item">B-</div>
+            <div class="rank-item">C+</div>
+          </div>
+        </div>
+      </div>
+    </main>
+  `,
+  methods: {},
+};
